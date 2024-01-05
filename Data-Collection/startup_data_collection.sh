86556d658434dd1111adaef1461607404d154284
TommiRkB <tommi.rainer@gmail.com>
2024-01-05 19:04:12 +0000

    Code commit v1

--- Data-Collection/startup_data_collection.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Start a new tmux session named "plant" + activate venv and run data_collection script
-tmux new-session -d -s plant "source /home/kaboom/Data_Collection/venv/bin/activate && python /home/kaboom/Data_Collection/data_collection.py"
-   
